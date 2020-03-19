b0a044a571b2383bf4a1ccebd7949d495cd315f8
52006894 <52006894@BLRIDC52006894.acsind.com>
2020-03-19 20:21:50 +0530

    commit

---
+++ BDDPOMFramework/src/main/java/CucumberPom/BDDPOMFramework/App.java
@@ -0,0 +1,13 @@
+package CucumberPom.BDDPOMFramework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
